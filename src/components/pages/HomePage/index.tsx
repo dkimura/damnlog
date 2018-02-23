be60cf1fa9d1a6fdfd3e3f7b0dcf6b9efecec3e9
Daichi Kimura <dkimura@bdd.xyz>
2018-02-24 00:18:43 +0900

    feat: Add PostList component

--- src/components/pages/HomePage/index.tsx
+++ src/components/pages/HomePage/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Head } from 'react-static'
 
 import { Hero, Title, Description } from 'components/parts/Hero'
+import { PostList } from 'components/parts/PostList'
 import { Template } from 'components/templates/Template'
 
 import { BasePageProps, Post } from 'types'
@@ -14,32 +15,18 @@ export const HomePage: React.StatelessComponent<Props> = ({
   handleHistoryPush,
   posts,
 }) => (
-    <Template onNavClick={handleHistoryPush}>
-      <Head>
-        <title>damnlog</title>
-        <meta name="description" content="Mine has been a life of much shame." />
-      </Head>
-      <Hero>
-        <Title>damnlog</Title>
-        <Description>Mine has been a life of much shame.</Description>
-      </Hero>
-      <main>
-        <h2>Recent Posts</h2>
-        <ul>
-          {posts.slice(0, 4).map(post => (
-            <li key={post.slug}>
-              <a
-                href={`/posts/${post.slug}/`}
-                onClick={event =>
-                  handleHistoryPush &&
-                  handleHistoryPush(event, `/posts/${post.slug}/`)
-                }
-              >
-                {post.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </Template>
-  )
+  <Template onNavClick={handleHistoryPush}>
+    <Head>
+      <title>damnlog</title>
+      <meta name="description" content="Mine has been a life of much shame." />
+    </Head>
+    <Hero>
+      <Title>damnlog</Title>
+      <Description>Mine has been a life of much shame.</Description>
+    </Hero>
+    <main>
+      <h2>Recent Posts</h2>
+      <PostList posts={posts.slice(0, 4)} onClick={handleHistoryPush} />
+    </main>
+  </Template>
+)
--- src/components/pages/PostsPage/index.tsx
+++ src/components/pages/PostsPage/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Head } from 'react-static'
 
 import { Template } from 'components/templates/Template'
+import { PostList } from 'components/parts/PostList'
 
 import { BasePageProps, Post } from 'types'
 import { Hero, Title, Description } from 'components/parts/Hero'
@@ -14,32 +15,17 @@ export const PostsPage: React.StatelessComponent<Props> = ({
   handleHistoryPush,
   posts,
 }) => (
-    <Template onNavClick={handleHistoryPush}>
-      <Head>
-        <title>Posts | damnlog</title>
-        <meta name="description" content="my damn logs..." />
-      </Head>
-      <Hero>
-        <Title>Posts</Title>
-        <Description>my damn logs...</Description>
-      </Hero>
-      <main>
-        <span>All Posts:</span>
-        <ul>
-          {posts.map(post => (
-            <li key={post.slug}>
-              <a
-                href={`/posts/${post.slug}/`}
-                onClick={event =>
-                  handleHistoryPush &&
-                  handleHistoryPush(event, `/posts/${post.slug}/`)
-                }
-              >
-                {post.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </main>
-    </Template>
-  )
+  <Template onNavClick={handleHistoryPush}>
+    <Head>
+      <title>Posts | damnlog</title>
+      <meta name="description" content="my damn logs..." />
+    </Head>
+    <Hero>
+      <Title>Posts</Title>
+      <Description>my damn logs...</Description>
+    </Hero>
+    <main>
+      <PostList posts={posts} onClick={handleHistoryPush} />
+    </main>
+  </Template>
+)
