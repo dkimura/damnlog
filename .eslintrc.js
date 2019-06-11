512b13cf9d55b1a38a8ef01cddfe65ed738d4246
Daichi Kimura <dkimura@bdd.xyz>
2019-06-12 00:04:18 +0900

    feat: Initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,45 @@
 module.exports = {
-  extends: 'react-tools'
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/prefer-interface': 'off',
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '**/*.config.{js,ts}',
+          '**/*.spec.{ts,tsx}',
+          '**/*.stories.{ts,tsx}',
+        ],
+      },
+    ],
+    'import/order': ['error', { 'newlines-between': 'always' }],
+    'import/prefer-default-export': false,
+    'no-return-assign': 0,
+    'react-hooks/rules-of-hooks': 'error',
+    // FixMe https://github.com/yannickcr/eslint-plugin-react/issues/1846
+    'react/button-has-type': 'off',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.tsx'] }],
+    'react/prop-types': false,
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.svg'],
+        paths: ['./src', './node_modules'],
+      },
+    },
+  },
 }
